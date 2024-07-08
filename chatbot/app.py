fd5189071c78f431c0561347592a506941ed1401
Karthikeyan Viswanathan <yankarto@outlook.com>
2024-07-08 08:52:07 +0530

    update key

--- chatbot/app.py
+++ chatbot/app.py
@@ -8,10 +8,16 @@ import os
 from dotenv import  load_dotenv
 
 import sys
-print(sys.path)
+# print(sys.path)
 
-os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
-os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
+# LANGCHAIN_API_KEY=""
+# OPENAI_API_KEY=""
+
+# os.environ['LANGCHAIN_API_KEY']=LANGCHAIN_API_KEY
+# os.environ['OPENAI_API_KEY']=OPENAI_API_KEY
+
+# os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
+# os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
 
 #langsmith tracking
 os.environ['LANGCHAIN_TRACING_V2']="true"
