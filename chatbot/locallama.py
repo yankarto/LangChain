8eca6ad6701369153a4922c84ce9b87cae828798
Karthikeyan Viswanathan <yankarto@outlook.com>
2024-07-09 08:59:20 +0530

    ollama local llm

--- chatbot/locallama.py
+++ chatbot/locallama.py
@@ -3,43 +3,25 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
-import streamlit as st
+from langchain_community.llms import Ollama
+
 import os
+import streamlit as st
 from dotenv import  load_dotenv
-
-import sys
-# print(sys.path)
-
 
 load_dotenv()
 
 LANGCHAIN_TRACING_V2="true"
 LANGCHAIN_ENDPOINT="https://api.smith.langchain.com"
-LANGCHAIN_PROJECT="OpenaAI"
-
-# LANGCHAIN_API_KEY=""
-# OPENAI_API_KEY=""
-
-# os.environ['LANGCHAIN_API_KEY']=LANGCHAIN_API_KEY
-# os.environ['OPENAI_API_KEY']=OPENAI_API_KEY
+LANGCHAIN_PROJECT="Ollama"
 
 
-##langsmith API KEY
-# os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
+print(os.getenv("LANGCHAIN_API_KEY"))
 
-##OpenAI API KEY
-# os.environ['OPENAI_API_KEY']=os.getenv("OPENAI_API_KEY")
-
-
-
-#langsmith project name
-os.environ['LANGCHAIN_PROJECT']=LANGCHAIN_PROJECT
-
-#langsmith tracking
+os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
 os.environ['LANGCHAIN_TRACING_V2']=LANGCHAIN_TRACING_V2
 os.environ['LANGCHAIN_ENDPOINT']=LANGCHAIN_ENDPOINT
-
-## Prompt Template
+os.environ['LANGCHAIN_PROJECT']=LANGCHAIN_PROJECT
 
 prompt=ChatPromptTemplate.from_messages(
     [
@@ -47,18 +29,21 @@ prompt=ChatPromptTemplate.from_messages(
         ("user","Question:{question}")
     ]
 )
-
+ 
 ## Streamlit framework
 
-st.title('Lang chain Demo with openAI')
+st.title('Lang chain Demo with Ollama')
 input_text=st.text_input("Search the topic you want")
 
-##OpenAI LLM
 
-llm=ChatOpenAI(model="gpt-3.5-turbo")
+##ollama Llama2
+
+llm=Ollama(model="llama3")
+
 output_parser=StrOutputParser()
-chain=prompt|llm|output_parser
 
+chain=prompt|llm|output_parser
+ 
 if input_text:
     st.write(chain.invoke({'question':input_text}))
     
